0dd48e19304cb9f164b28143499279b5e6f9533c
aymansyam <aymansyam97@gmail.com>
2023-06-08 16:45:35 +0300

    code cleanup

--- mewa/mewa/spiders/general_spider.py
+++ mewa/mewa/spiders/general_spider.py
@@ -1,9 +1,6 @@
 import scrapy
-from selenium import webdriver
 from scrapy.selector import Selector
 from time import sleep
-
-JAVASCRIPT_LOAD_TIME = 5
 
 class GeneralSpider(scrapy.Spider):
     name = "general"
@@ -11,29 +8,14 @@ class GeneralSpider(scrapy.Spider):
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
 
-    # all pages use JavaScript and need selenium to load.
-    # [TODO] use other framework besides selenium to increase performance.
-    def selenium_get(self, response):
-        self.driver.get(response.url)
-
-        # Add a delay for JavaScript to load (change depending on machine performance)
-        # If you are getting null items, increase the sleep duration
-        sleep(JAVASCRIPT_LOAD_TIME)
-
-        html = self.driver.page_source
-        resp = Selector(text=html)
-        return resp
-    
-    # def __init__(self):
-        # self.driver = webdriver.Chrome()
+    allowed_domains = ['www.mewa.gov.sa']
 
     def start_requests(self):
         url = "https://www.mewa.gov.sa/ar/Pages/default.aspx"
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # public store landing page and categories
-        # resp = self.selenium_get(response)
+        # recursive access to website links
         href_elements = response.css('a::attr(href)')
         for href in href_elements:
             try:
