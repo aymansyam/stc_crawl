f4f8ac5d7249c02e5667d9d9b0f63793b104fdc6
aymansyam <aymansyam97@gmail.com>
2023-06-08 14:51:15 +0300

    completed crawler

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # stc_crawl
+
+To start the crawler, first install the dependencies using Pip:
+``` 
+$ pip install -r requirements.txt
+```
+
+Go to project directory
+```
+$ cd stc
+```
+
+Start the spider
+```
+$ scrapy crawl products
+```
+
+You will get a file called data.json at the project root which will have the scrapped data. You can change the output to any supported format by scrapy as such:
+
+```
+$ scrapy crawl products -O output.csv
+```
+
+This will output the file in csv format.
+
+If you are getting any null items, increase sleep duration under selenium_get() method. Otherwise, you can decrease for better performance.
---
+++ stc/stc/spiders/products_spider.py
@@ -0,0 +1,66 @@
+import scrapy
+from selenium import webdriver
+from scrapy.selector import Selector
+from time import sleep
+
+class ProductsSpider(scrapy.Spider):
+    name = "products"
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'FEEDS': { 'data.json': { 'format': 'json'}}
+    }
+
+    allowed_domains = ["www.stc.com.sa"]
+
+    # all pages use JavaScript and need selenium to load.
+    # [TODO] use other framework besides selenium to increase performance.
+    def selenium_get(self, response):
+        self.driver.get(response.url)
+
+        # Add a delay for JavaScript to load (change depending on machine performance)
+        # If you are getting null items, increase the sleep duration
+        sleep(5)
+
+        html = self.driver.page_source
+        resp = Selector(text=html)
+        return resp
+    
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def start_requests(self):
+        url = "https://www.stc.com.sa/content/stc-public-store/sa/ar/public-store-landing-page.html"
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        # public store landing page and categories
+        resp = self.selenium_get(response)
+
+        elements = resp.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "cmp-ShopCards__button contentWrapperCategory RTL", " " ))]/ul/li')
+        for element in elements:
+            link = element.xpath('./a/@href').get()
+            if link is not None:
+                yield response.follow(link, self.parse_link)
+
+    
+    def parse_link(self, response):
+        # product list
+        resp = self.selenium_get(response)
+
+        elements = resp.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "card-link", " " ))]/@href').getall()
+        yield from response.follow_all(elements, self.parse_product)
+
+    def parse_product(self, response):
+        # product description
+        resp = self.selenium_get(response)
+
+        title = resp.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "cmp-product-name", " " ))]/text()').get()
+        yield {
+            'title': title
+        }
+
+
+
+        
+
+
